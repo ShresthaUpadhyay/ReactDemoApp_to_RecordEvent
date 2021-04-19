edf55e1a73b76f94523af2922ccec36655f577de
Shrestha <harry143lover@gmail.com>
2021-04-19 15:00:12 +0530

    payload task

--- App.js
+++ App.js
@@ -6,119 +6,121 @@
  * @flow strict-local
  */
 
-import React from 'react';
-import type {Node} from 'react';
+import React  from 'react';
 import {
   SafeAreaView,
   ScrollView,
   StatusBar,
   StyleSheet,
   Text,
+  Button,
   useColorScheme,
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+// import {
+//   Colors,
+//   DebugInstructions,
+//   Header,
+//   LearnMoreLinks,
+//   ReloadInstructions,
+// } from 'react-native/Libraries/NewAppScreen';
 
 const CleverTap = require('clevertap-react-native');
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+ 
+export default class App extends React.Component{
   
-};
+  constructor(props){
+    super(props);
+      this.state={
+        title:"",
+        message: " "
+      };
+      
+  }
+  
+  
 
-CleverTap.onUserLogin({'Name': 'ShreReact', 'Identity': '665665', 'Email': 'shres@R.com', 'custom1': 123});
+  componentDidMount(){
+    CleverTap.onUserLogin({'Name': 'ShreReact', 'Identity': '665665', 'Email': 'shres@R.com', 'custom1': 123});
 
-CleverTap.profileGetCleverTapID((err, res) => {
-  console.log('CleverTapID', res, err);
-});
 
-CleverTap.createNotificationChannel("123","CleverTap","This is a test channel",5,true);
+    CleverTap.createNotificationChannel("123","CleverTap","This is a test channel",5,true);
+
+    console.log('notification channel created');
+    
+    
+
+    //adding listeners            event name                    ,        handler function
+    CleverTap.addListener(CleverTap.CleverTapDisplayUnitsLoaded, (event) => {this._handleCleverTapDisplayUnitsLoaded(CleverTap.CleverTapDisplayUnitsLoaded,event);});
+    
+    
+  }
+
+  _handleCleverTapDisplayUnitsLoaded(eventName,event){
+
+    console.log("payload for ",eventName," >>>> ",event);
+    
+    var value = event.displayUnits[0];  
+    value = value.replace("[", " ");
+    value = value.replace("]", " ");
+    var Obj = JSON.parse(value);
+
+    this.setState({title:Obj.content.title["text"],message : Obj.content.message["replacements"]});
+  
+    
+    
+    // console.log("Title of the payload:",obj.content.title["text"]);  // to print the payload data to console
+    // console.log("Message of the payload : ",obj.content.message["og"]);
+
+    
+    // var a = 1;
+    // for (const [key, value] of Object.entries(event)) {
+    //   console.log(`${key}: ${value}`);
+      //}
+      // var str = JSON.stringify(event,null,"\t");
+    // console.log(str);
+    // eventdata = (obj,props)=>{
+    //   return <h1>{props.obj}</h1>
+    // }
+  }
+   
+ 
+
+  componentWillUnmount(){
+    CleverTap.profileGetCleverTapID((err, res) => {
+      console.log('CleverTapID', res, err);
+    });
+    CleverTap.removeListener(CleverTap.CleverTapDisplayUnitsLoaded);
+    console.log('removing listeners....')
+  }
+  
+  
+  render(){
+    return (
+      <View style = {styles.container}>
+        <Text>Title of the payload : {this.state.title}</Text>
+        <Text>Message of the payload : {this.state.message}</Text> 
+      </View>
+      );
+    
+    };
+}
+
 
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+  container: {
+    flex: 1,
+    fontWeight: 'bold',
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+ },
 });
 
-export default App;
+
